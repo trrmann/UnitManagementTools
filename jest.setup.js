7cefad14c9e239edfe5a9996821607072de6f436
trrmann <113477307+trrmann@users.noreply.github.com>
2026-01-02 22:46:58 -0500

    clean up external dependancies

--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-// Polyfill TextEncoder and TextDecoder for Node.js/jsdom environments
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = require('util').TextEncoder;
-}
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = require('util').TextDecoder;
-}
